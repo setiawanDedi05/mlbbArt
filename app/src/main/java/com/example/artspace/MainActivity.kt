5f50c3669900ec8eca6477a0c2ced397b9e34aa2
Dedi Setiawan <dedi.setiawan@primecash.co.id>
2023-10-12 09:31:42 +0700

    add image

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -22,9 +25,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,6 +55,9 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ArtSpaceApp() {
+    var currentHero by remember{
+        mutableStateOf(2)
+    }
     Scaffold(
         topBar = {
         CenterAlignedTopAppBar(
@@ -71,40 +84,77 @@ fun ArtSpaceApp() {
         ){
             Column(
                 modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
             ) {
-                HeroImageAndText()
+                when(currentHero){
+                    1 -> HeroImageAndText(image = R.drawable.chou, title = R.string.chou)
+                    2 -> HeroImageAndText(image = R.drawable.gs, title = R.string.gusion)
+                    3 -> HeroImageAndText(image = R.drawable.angela, title = R.string.angela)
+                    4 -> HeroImageAndText(image = R.drawable.claude, title = R.string.claude)
+                    5 -> HeroImageAndText(image = R.drawable.floryn, title = R.string.floryn)
+                    6 -> HeroImageAndText(image = R.drawable.clint, title = R.string.clint)
+                    7 -> HeroImageAndText(image = R.drawable.johnson, title = R.string.johnson)
+                    8 -> HeroImageAndText(image = R.drawable.khufra, title = R.string.khufra)
+                    9 -> HeroImageAndText(image = R.drawable.lancelot, title = R.string.lancelot)
+                    10 -> HeroImageAndText(image = R.drawable.leomord, title = R.string.leomord)
+                    else -> HeroImageAndText(image = R.drawable.xavier, title = R.string.xavier)
+                }
                 Spacer(modifier = Modifier.height(10 .dp))
-                ButtonControll()
+                ButtonControll(
+                    onPrev = {
+                            currentHero--
+                    },
+                    onNext = {
+                            currentHero++
+                    },
+                    isPrevEnable = currentHero != 1,
+                    isNextEnable = currentHero != 11
+                )
             }
         }
     }
 }
 
 @Composable
-fun ButtonControll(){
+fun ButtonControll(
+    isPrevEnable : Boolean,
+    isNextEnable : Boolean,
+    onPrev : () -> Unit,
+    onNext : () -> Unit
+){
     Row() {
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Previous")
+        Button(onClick = onPrev, enabled = isPrevEnable) {
+            Text(text = stringResource(R.string.previous))
         }
         Spacer(modifier = Modifier.width(10 .dp))
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Next")
+        Button(onClick = onNext, enabled = isNextEnable) {
+            Text(text = stringResource(R.string.next))
         }
     }
 }
 
 @Composable
-fun HeroImageAndText(){
+fun HeroImageAndText(
+    @StringRes title : Int,
+    @DrawableRes image: Int
+    ){
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+    val screenHeight = configuration.screenHeightDp.dp
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Image(
-            painter = painterResource(id = R.drawable.chou),
-            contentDescription = "Chou"
+            painter = painterResource(image),
+            contentDescription = stringResource(id = title),
+            modifier = Modifier
+                .width(screenWidth.times(0.8f))
+                .height(screenHeight.times(0.5f)),
+            contentScale = ContentScale.Crop
         )
         Spacer(modifier = Modifier.height(10 .dp))
-        Text(text = "Chou")
+        Text(text = stringResource(id = title))
     }
 }
+
